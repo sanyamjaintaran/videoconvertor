05b730f86974b94d43c4edcaa55973bc749782d3
sanyamjaintaran <sanyamj449@gmail.com>
2026-07-10 21:21:21 +0530

    Implement image preview and audio file info display
    Add functionality to preview selected image and display audio file info.

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
+const imageInput = document.getElementById("imageInput");
 
+const audioInput = document.getElementById("audioInput");
+
+const imagePreview = document.getElementById("imagePreview");
+
+const audioInfo = document.getElementById("audioInfo");
+
+imageInput.addEventListener("change",()=>{
+
+const file=imageInput.files[0];
+
+if(!file) return;
+
+const reader=new FileReader();
+
+reader.onload=function(e){
+
+imagePreview.innerHTML=`<img src="${e.target.result}">`;
+
+}
+
+reader.readAsDataURL(file);
+
+});
+
+audioInput.addEventListener("change",()=>{
+
+const file=audioInput.files[0];
+
+if(!file) return;
+
+audioInfo.innerHTML=`
+
+<b>Name :</b> ${file.name}<br>
+
+<b>Size :</b> ${(file.size/1024/1024).toFixed(2)} MB
+
+`;
+
+});
